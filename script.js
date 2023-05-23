8738387e8e0162350ccfb09e808a74ee9a298b17
georgefrg <georgeftw00@gmail.com>
2023-05-23 12:42:31 +0300

    change color when negative or positive

--- script.js
+++ script.js
@@ -19,6 +19,8 @@ buttons.forEach(function (btn) {
       value.style.color = "green";
     } else if (count < 0) {
       value.style.color = "red";
+    } else {
+      value.style.color = "black";
     }
     value.textContent = count;
   });
